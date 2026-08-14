4782263720afefc33c983a515df1b08c84956bcf
mbo <mbo.kastnev@gmail.com>
2026-08-15 02:30:43 +0300

    refactor

--- app/init.cpp
+++ app/init.cpp
@@ -2,6 +2,8 @@
 /*  Includes                                                             */
 /*=======================================================================*/
 #include "init.h"
+#include "FreeRTOS.h"
+#include "task.h"
 #include "startup/cp15.h"
 #include "regs/REGS.hpp"
 #include "rtt/rtt_log.h"
@@ -9,7 +11,6 @@
 #include "hal/sysTimer.hpp"
 #include "hal/boards/beaglebone_black.hpp"
 #include "hal/MMU.hpp"
-
 
 #define TAG "brd_ini"
 
@@ -94,6 +95,15 @@ extern "C" void vApplicationIRQHandler(void)
     // Очистить прерывание в INTC
     volatile uint32_t *control = (volatile uint32_t *)0x48200048;
     *control = 0x1; // NEWIRQAGR
+}
+
+extern "C" void vApplicationStackOverflowHook(TaskHandle_t xTask, const char *pcTaskName)
+{
+    (void)xTask;
+    (void)pcTaskName;
+    // Вход сюда означает, что стек задачи pcTaskName зажван!
+    __asm volatile("bkpt #0");
+    while(true){}
 }
 
 struct alignas(8) FaultContext
